e1de1e7db98e8ac752a44a82a2622d2c0066a101
= <=>
2023-02-03 12:44:09 -0700

    created content list and content componenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Title from './Title'
+import Title from './components/Title'
+import POPOSList from './components/POPOSList';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
